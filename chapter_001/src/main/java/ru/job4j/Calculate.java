c0aca03e45ca407716d3135fb49d5d1e6b1c9def
Ateo13 <xxxAteoxxc@gmail.com>
2018-01-02 22:18:09 +0300

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -6,14 +6,21 @@ package ru.job4j;
 * @version 1.0
 */
 
-public class Calculate 
+public class Calculate {
 /**
 * Главный метод, вывод текста на экран
 * @param arg - arg
 */
-{
     public static void main(String[] args)
         {
         System.out.println("Hello World");
          }
+/**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
---
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculateTest task.
+*
+* @author atiurin (xxxAteoxxc@gmail.com)
+* @version 1.0
+* @since 01.01.2018
+*/
+package ru.job4j;

341f97f951b7e91c7ee71fdd2110f7a1c3ceff15
Ateo13 <xxxAteoxxc@gmail.com>
2018-01-20 19:44:02 +0300

    4.1. Подсчет суммы чётных чисел в диапазоне [#192] - доделано

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -1,6 +1,16 @@
 package ru.job4j.loop;
-
+/**
+ * @author atiurin
+ * @since 19.01.2018
+ * @version 1.0
+ */
 public class Counter {
+    /**
+     * Метод суммирует все четные числа в диапазоне
+     * @param start
+     * @param finish
+     * @return
+     */
     public int add(int start, int finish) {
         int result = 0;
         for (int i = start; i <= finish; i++) {
--- chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -1,10 +1,13 @@
 package ru.job4j.loop;
 
 import org.junit.Test;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-
+/**
+ * @author atiurin
+ * @since 19.01.2018
+ * @version 1.0
+ */
 public class CounterTest {
     @Test
     public void whenSumEvenNumbersFromOneToTwelveThenFourtyTwo() {

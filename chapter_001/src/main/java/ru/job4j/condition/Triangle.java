40813080bd420c314aeccab6ad9761fcfc5d18df
Ateo13 <xxxAteoxxc@gmail.com>
2018-01-20 19:17:53 +0300

    4.1. Подсчет суммы чётных чисел в диапазоне [#192] - исправлено

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -20,7 +20,7 @@ public class Triangle {
      * @param ab расстояние между точками a b
      * @param ac расстояние между точками a c
      * @param bc расстояние между точками b c
-     * @return Перимент.
+     * @return Периметр.
      */
     public double period(double ab, double ac, double bc) {
         return (ab + ac + bc) / 2;
@@ -31,12 +31,13 @@ public class Triangle {
      * @return Вернуть площадь, если треугольник существует или -1, если треугольника нет.
      */
     public double area() {
+        double rsl = -1;
         double ab = this.a.distanceTo(this.b);
         double ac = this.a.distanceTo(this.c);
         double bc = this.b.distanceTo(this.c);
         double p = this.period(ab, ac, bc);
-        double rsl = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
         if (this.exist(ab, ac, bc)) {
+            rsl = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
         }
         return rsl;
     }
@@ -51,9 +52,7 @@ public class Triangle {
      * @return
      */
     private boolean exist(double ab, double ac, double bc) {
-        if ((ab + ac < bc) && (ab + bc < ac) && (bc + ac < ab)) {
-        }
-        return false;
+        return (ab + ac > bc) && (ab + bc > ac) && (bc + ac > ab);
     }
 }
 
--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -2,15 +2,13 @@ package ru.job4j.loop;
 
 public class Counter {
     public int add(int start, int finish) {
-
+        int result = 0;
         for (int i = start; i <= finish; i++) {
             if (i % 2 == 0) {
-                int result = 0;
                 result = i + result;
-                return (result);
             }
         }
-        // сюда он хочет return, но тогда туда не получится вставить result
+        return (result);
     }
 }
 

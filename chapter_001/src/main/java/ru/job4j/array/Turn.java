d3cd99ec423730696e81f1b9ac6c608305f7d7e1
Ateo13 <xxxAteoxxc@gmail.com>
2018-02-06 23:44:50 +0300

    5.2. Перевернуть массив. [#4441][#39590]

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -1,11 +1,21 @@
 package ru.job4j.array;
-
+/**
+ * @author atiurin (xxxAteoxxc@gmail.com)
+ * @since 06.02.2018
+ * @version 1.0
+ */
 public class Turn {
-    public int[] back(int[] array)
-    {
-        for (int i = 0; i < array.length; i++) {
-                array[i] = array[array.length - 1 - i];
-    }
+    /**
+     * Метод переворачивает массив
+     * @param array
+     * @return
+     */
+    public int[] back(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i -1] = temp;
+        }
         return array;
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/TurnTest.java
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -1,7 +1,10 @@
 package ru.job4j.array;
-
+/**
+ * @author atiurin (xxxAteoxxc@gmail.com)
+ * @since 06.02.2018
+ * @version 1.0
+ */
 import org.junit.Test;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -9,14 +12,17 @@ public class TurnTest {
     @Test
     public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
         Turn turn = new Turn();
-        int [] res = {2, 4, 6, 8};
-        int [] result = turn.back(res);
-        int[] expected = {8, 6, 4 ,2};
+        int[] res = {2, 4, 6, 8};
+        int[] result = turn.back(res);
+        int[] expected = {8, 6, 4, 2};
         assertThat(result, is(expected));
     }
-
     @Test
     public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
-        //напишите здесь тест, проверяющий переворот массива с нечётным числом элементов, например {1, 2, 3, 4, 5}.
+        Turn turn = new Turn();
+        int[] res = {1, 2, 3, 4, 5};
+        int[] result = turn.back(res);
+        int[] expected = {5, 4, 3, 2, 1};
+        assertThat(result, is(expected));
     }
 }

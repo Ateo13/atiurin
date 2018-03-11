d82525ecdfe94d5212c298166e019f514defcdcb
Ateo13 <xxxAteoxxc@gmail.com>
2018-03-11 22:43:29 +0300

    Проверка вхождения слова - доделка2

--- chapter_001/src/main/java/ru/job4j/prov/StringFinder.java
+++ chapter_001/src/main/java/ru/job4j/prov/StringFinder.java
@@ -2,11 +2,12 @@ package ru.job4j.prov;
 /**
  * @author atiurin (xxxAteoxxc@gmail.com)
  * @since 12.02.2018
- * @version 1.1
+ * @version 1.2
  */
 public class StringFinder {
     /**
      * Метод проверяет, находится ли одно слово в другом
+     *
      * @param origin
      * @param sub
      * @return
@@ -17,11 +18,14 @@ public class StringFinder {
         boolean result = false;
         for (int i = 0; i < orig.length; i++) {
             for (int j = 0; j < s.length; j++) {
-                if (orig[i] == s[j] && orig[orig.length - 1] == s[s.length - 1]) {
+                if (orig[i] == s[j])
+                {
+                    if (orig[i] != s[j])
+                        break;
                     result = true;
                 }
             }
-        }
+            }
         return result;
     }
 }
--- chapter_001/src/test/java/ru/job4j/prov/StringFinderTest.java
+++ chapter_001/src/test/java/ru/job4j/prov/StringFinderTest.java
@@ -2,7 +2,7 @@ package ru.job4j.prov;
 /**
  * @author atiurin (xxxAteoxxc@gmail.com)
  * @since 12.02.2018
- * @version 1.1
+ * @version 1.2
  */
 import org.junit.Test;
 
@@ -12,14 +12,14 @@ public class StringFinderTest {
     @Test
     public void stringOneIncludeStringTwo() {
         StringFinder find = new StringFinder();
-        boolean result = find.contains("Зачемтаксложно", "сложно");
+        boolean result = find.contains("Зачемтаксложно", "сложн");
         boolean excepted = true;
         assertThat(result, is(excepted));
     }
     @Test
     public void stringOneNoIncludeStringTwo() {
         StringFinder find = new StringFinder();
-        boolean result = find.contains("Утка", "Коко");
+        boolean result = find.contains("Зачемтаксложно", "ъ");
         boolean excepted = false;
         assertThat(result, is(excepted));
     }
